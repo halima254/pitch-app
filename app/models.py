5646e50571f03c6868a7eeadbb3635e3def14753
halima254 <halima.yahya@student.moringaschool.com>
2021-09-21 16:44:29 +0300

    modify files

--- app/models.py
+++ app/models.py
@@ -1,36 +1,84 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash,check_password_hash
-from app import login
+from . import login_manager
 from flask_login import UserMixin
 
 class User(UserMixin,db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),index=True, unique=True)
     email = db.Column(db.String(120),index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    pitch = db.relationship('Pitch', backref='author', lazy='dynamic')
+    comment = db.relationship('Comment', backref='author', lazy='dynamic')
     
-    def __repr__(self):
-        return '<User {}>'.format(self.username) 
-    
-    def set_password(self, password):
+    @property
+    def password(self):
+        raise AttributeError('You cannnot read the password attribute')
+
+    @password.setter
+    def password(self, password):
         self.password_hash = generate_password_hash(password)
-        
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)   
-    
-   
-    
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return f'User {self.username}' 
     
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))  
+    @login_manager.user_loader
+    def load_user(author_id):
+        return User.query.get(int(author_id))
+    
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    content = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    comment = db.relationship('Comment', backref='pitch_id', lazy='dynamic')
+    likes = db.Column(db.Integer)
+    dislikes = db.Column(db.Integer)
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_pitches(cls, category):
+        pitches = Pitch.query.filter_by(category = category).all()
+        return pitches
+    
+    @classmethod 
+    def get_pitch(cls, id):
+        pitch = pitch.query.filter_by(id = id).first()
+        return pitch
+        
+        
+class Comment(db.Model):
+    __tablename__ = 'comments' 
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    
+    def save_comments(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_comments(cls, pitch):
+        comments = Comment.query.filter_by(pitch_id = pitch).all()
+        return comments  () 
+          
+    
+ 
+ 
+    
+# @login.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))  
--- config.py
+++ config.py
@@ -1,10 +1,46 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysterious'
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/pitching'
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:access@localhost/pitching'
+    DEBUG = True
+    
+config_options = {
+        'development':DevConfig,
+        'production':ProdConfig
+    }   
