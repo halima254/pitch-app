d769075644e820e2eab9f4b1b98e84df8956ca82
halima254 <halima.yahya@student.moringaschool.com>
2021-09-22 22:56:11 +0300

    title

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,7 +18,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Pitch first login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
@@ -36,7 +36,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch App","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
